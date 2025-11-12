ed1937ab403bf8758ceee1fdc09525da2b504b32
akthernahela <nahela00@gmail.com>
2025-11-12 19:08:35 +0100

    A couple of braces drove me crazy

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -41,7 +41,7 @@ export default function Main() {
                 <h1>Attrici ed Attori</h1>
                 <div className="container">
                     {
-                        allActors.map(person => {
+                        allActors.map(person => (
                             <div className="card mb-3" key={person.id}>
                                 <div className="row g-0">
                                     <div className="col-md-4">
@@ -58,7 +58,7 @@ export default function Main() {
                                     </div>
                                 </div>
                             </div>
-                        })
+                        ))
                     }
                 </div>
             </main>
