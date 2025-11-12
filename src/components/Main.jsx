40035e1db60067762ca23b55ee737860bad8d102
akthernahela <nahela00@gmail.com>
2025-11-12 12:38:42 +0100

    Try to get data from axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,22 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function Main() {
+    const [actress, setactress] = useState([])
     const actressList = 'https://lanciweb.github.io/demo/api/actresses/'
     const actorList = 'https://lanciweb.github.io/demo/api/actors/'
+    function printPics() {
+        axios
+            .get(actressList)
+            .then(response => {
+                console.log(response.data.results);
 
-    return
+            })
+    }
+    return (
+        <>
+            <main>
+
+            </main>
+        </>
+    )
 }
