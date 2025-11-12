fcca6977c97f76f86b1343e6d56e92aa86cc50b5
akthernahela <nahela00@gmail.com>
2025-11-12 12:25:56 +0100

    Add two variable for the actor and actress list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 
 export default function Main() {
-
+    const actressList = 'https://lanciweb.github.io/demo/api/actresses/'
+    const actorList = 'https://lanciweb.github.io/demo/api/actors/'
     return
 }
