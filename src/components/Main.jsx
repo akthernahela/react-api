9d083f732813770685e7ed1aaa750a00066e3d57
akthernahela <nahela00@gmail.com>
2025-11-12 12:27:56 +0100

    Import the useEffect from react

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function Main() {
     const actressList = 'https://lanciweb.github.io/demo/api/actresses/'
     const actorList = 'https://lanciweb.github.io/demo/api/actors/'
+
     return
 }
